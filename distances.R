a536da7dce8817167322b114261ec915d044c2b8
MCooperBorkenhagen <matthew.borkenhagen@gmail.com>
2023-06-23 09:59:01 -0500

    ...

--- distances.R
+++ distances.R
@@ -30,6 +30,5 @@ distance_matrix_to_long = function(m, words = NA){
       left_join(indices, by = c("column_index" = "index")) %>% 
       select(word_1 = word.x, word_2 = word.y, distance)
 
-   return(long_format)}
-  }
+   return(long_format)}}
 
